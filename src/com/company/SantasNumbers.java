b8a7931734cac18df66b0043b8bfcf96aed03b59
Daryna Buzovska <buzovskadaryna@gmail.com>
2017-09-04 13:47:13 +0300

    first commit

--- src/com/company/SantasNumbers.java
+++ src/com/company/SantasNumbers.java
@@ -3,5 +3,28 @@ package com.company;
 /**
  * Created by user on 04.09.2017
  */
+import java.util.ArrayList;
+
 public class SantasNumbers {
+
+    public static int findNumber(int start, int number) {
+        int[] array = new int[number];
+        for (int i = 1; i <= number; i++) {
+            array[i - 1] = i;
+        }
+
+        ArrayList<Integer> primes = new ArrayList<Integer>();
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] != 0) {
+                for (int j = array[i]*2-1; j < array.length; j+=array[i]) {
+                    array[j] = 0;
+                }
+                String str = array[i] + "";
+                if (!str.contains("13") && array[i] > start)
+                primes.add(array[i]);
+            }
+        }
+        return primes.size();
+    }
 }
+
