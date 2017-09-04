6d0710445210f6a553fc5c7d2bac1e48d8625c14
Daryna Buzovska <buzovskadaryna@gmail.com>
2017-09-04 13:46:07 +0300

    everything ok

---
+++ src/com/company/SantasNumbers.java
@@ -0,0 +1,7 @@
+package com.company;
+
+/**
+ * Created by user on 04.09.2017
+ */
+public class SantasNumbers {
+}
---
+++ test/java/com/company/SantasNumbersTest.java
@@ -0,0 +1,8 @@
+import static org.junit.Assert.*;
+
+/**
+ * Created by user on 04.09.2017
+ */
+public class SantasNumbersTest {
+
+}
