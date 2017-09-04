585a266cf7bc73be00810f6170f0eea53f615310
Daryna Buzovska <buzovskadaryna@gmail.com>
2017-09-04 13:48:28 +0300

    first commit

--- test/java/com/company/SantasNumbersTest.java
+++ test/java/com/company/SantasNumbersTest.java
@@ -1,8 +1,16 @@
-import static org.junit.Assert.*;
+package com.company;
+
+import org.junit.Assert;
 
 /**
  * Created by user on 04.09.2017
  */
 public class SantasNumbersTest {
+    @org.junit.Test
+    public void findNumber() throws Exception {
+        Assert.assertEquals(4, SantasNumbers.findNumber(9,23));
+        Assert.assertEquals(3, SantasNumbers.findNumber(18,30));
+        Assert.assertEquals(4, SantasNumbers.findNumber(18,31));
+    }
 
 }
